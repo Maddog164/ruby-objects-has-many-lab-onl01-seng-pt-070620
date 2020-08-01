e2a0bd7c6d75f27b3192685b75c374deee04b44b
Denise Henderson <denise@henderson3.us>
2020-08-01 00:32:13 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -3,9 +3,16 @@ class Post
 
   attr_accessor :title, :author
 
+  @@all = []
+
   def initialize
     @title = title
     @author = author
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+    #binding.pry
   end
 
 end
